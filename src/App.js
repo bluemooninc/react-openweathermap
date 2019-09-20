12d0d77e82b11c10ea3ba4afe3c7f0ff96e9d12a
Yoshi Sakai <yoshi.sakai@gmail.com>
2019-09-20 17:46:03 +0900

    Add input field

--- src/App.js
+++ src/App.js
@@ -8,20 +8,21 @@ export default class extends React.Component {
         super(props);
         this.state = {
             apiKey : 'YOUR_API_KEY',
-            place : 'Tokyo,jp',
+            requestCity: '',         //  ex. 'Tokyo,jp'
             city: '',
             response : []
         };
+        this.handleInput = this.handleInput.bind(this);
+        this.handleGetWeather = this.handleGetWeather.bind(this);
     }
     handleGetWeather(){
         axios
             .get(API_ENDPOINT, {
                 params: {
-                    q: this.state.place,
+                    q: this.state.requestCity,
                     APPID: this.state.apiKey
                 } })
             .then(res => {
-                console.log(res);
                 this.setState({
                     response: res.data.list,
                     city: res.data.city.name
@@ -31,21 +32,24 @@ export default class extends React.Component {
                 console.log(error);
             });
     }
+    handleInput({ target: { value } }) {
+        this.setState({
+            requestCity: value
+        });
+    }
     render() {
         console.log(this.state.response);
+
         return (
             <div>
-                <h1 className="app-title">Weather report</h1>
-                <input
-                    type="button"
-                    value="Search"
-                    onClick={() => this.handleGetWeather()}
-                />
+                <h1>Weather forecast</h1>
+                <input type="text" value={this.state.requestCity} onChange={this.handleInput} />
+                <button onClick={this.handleGetWeather}>Search</button>
                 <p> Location: {this.state.city} </p>
                 {Object.keys(this.state.response).map(key => (
                     <li key={key}>
                         {this.state.response[key].dt_txt}
-                        ,
+                        ,<img src={'http://openweathermap.org/img/w/'+this.state.response[key].weather[0].icon+'.png'} />
                         {this.state.response[key].weather[0].main}
                     </li>
                 ))}
